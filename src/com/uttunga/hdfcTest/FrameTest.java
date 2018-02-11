9b89f9ba510bc9a74b4edc61c19433330b62af0f
Sreenivasamurthy <shreehari.sreenivasamurthy@honeywell.com>
2018-02-11 15:38:23 +0530

    New changes

--- src/com/uttunga/hdfcTest/FrameTest.java
+++ src/com/uttunga/hdfcTest/FrameTest.java
@@ -12,6 +12,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FrameTest {
 	static WebDriver driver1; 
 
+	// COmments
+	//
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -89,6 +92,8 @@ public class FrameTest {
 		}
 	}
 	
+	//
+	//
 	
 	public static void frameType(By Locator, String value)
 	{
